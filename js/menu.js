34ca270cca028a59b4f559716d66f9b48176228f
Denilson Junior <dehjr1990@gmail.com>
2023-08-21 22:49:14 -0300

    [REFACTURE]

--- js/menu.js
+++ js/menu.js
@@ -8,23 +8,23 @@ menuShow();
 handleResize();
 
 function menuShow() {
-    closed.addEventListener('click', () => toggleMenu(true));
-    open.addEventListener('click', () => toggleMenu(false));
+    open.addEventListener('click', () => toggleMenu(true));
+    closed.addEventListener('click', () => toggleMenu(false));
 }
 
 function toggleMenu(openMenu) {
-    closed.classList.toggle('hide', openMenu);
-    open.classList.toggle('hide', !openMenu);
+    open.classList.toggle('hide', openMenu);
+    closed.classList.toggle('hide', !openMenu);
     navbar.style.display = openMenu ? 'flex' : 'none';
 }
 
 function handleResize() {
     const larguraViewport = window.innerWidth;
-    
+
     if (larguraViewport < 768) {
+        toggleMenu(false);
         navbar.style.display = 'none';
         navgator.forEach(item => item.addEventListener("click", verificaNav));
-        console.log(larguraViewport);
     } else {
         navbar.style.display = 'flex';
         navgator.forEach(item => item.removeEventListener("click", verificaNav));
